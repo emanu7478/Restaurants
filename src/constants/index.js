673b7e8ae568e1a9a84e4af7e8643b693819120d
emanu7478 <emanu7478@gmail.com>
2021-04-09 02:17:17 +0530

    add constants

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+export const APP_NAME = 'Restaurants';
+
+//Colors
+export const PRIMARY_COLOR = '#ffffff';
+export const SECONDARY_COLOR = '#0077FF';
+export const TERTIARY_COLOR = '#2e2e2e';
+export const QUARTERNARY_COLOR = '#535353';
+export const GREY_COLOR = '#DADADA';
+export const BACKGROUND_PRIMARY_COLOR = '#F4F8FF';
+
+//fonts
+// export const PRIMARY_FONT = 'NunitoSans-Bold';
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {View} from 'react-native';
+// import RNTextInput from './components/RNTextInput';
 
 function SufficioIndex() {
-  return <View />;
+  return <View>{/* <RNTextInput /> */}</View>;
 }
 
 export default SufficioIndex;
