839f9a97558521c009e46ad2e089b2cc423dd6c6
emanu7478 <emanu7478@gmail.com>
2021-04-09 23:55:56 +0530

    add home screen UI

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,6 +8,8 @@ export const QUARTERNARY_COLOR = '#535353';
 export const GREY_COLOR = '#DADADA';
 export const BACKGROUND_PRIMARY_COLOR = '#F4F8FF';
 export const SMOKE_WHITE = 'rgb(245,248,253)';
+export const BLUR_WHITE = 'rgba(255, 255, 255, .7)';
+export const GLOSSY_WHITE = '#f2f4f9';
 
 //fonts
 // export const PRIMARY_FONT = 'NunitoSans-Bold';
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,8 +1,86 @@
 import React, {useEffect, useState} from 'react';
-import {View} from 'react-native';
+import {
+  FlatList,
+  TouchableHighlight,
+  useWindowDimensions,
+  View,
+  Image,
+  Text,
+} from 'react-native';
+import {Icon} from 'react-native-elements';
+import axios from 'axios';
+import style from './style';
 
 function Home() {
-  return <View style={{flex: 1}} />;
+  const {width} = useWindowDimensions();
+  const [restaurants, updateRestaurants] = useState([]);
+  const [restaurantSearchResult, updateRestaurantSearchResult] = useState([]);
+
+  const renderRestaurants = ({item}) => {
+    return (
+      <TouchableHighlight>
+        <View style={{...style.restaurantContainer, width: width - 15}}>
+          <View>
+            <Image
+              style={style.restaurantImage}
+              source={require('../../assets/Images/restaurant.jpeg')}
+            />
+            <View style={style.cuisineTypeContainer}>
+              <Text style={style.cuisineType}>{item.cuisine_type}</Text>
+            </View>
+          </View>
+          <View style={style.restaurantDetailContainer}>
+            <View style={style.restaurantDetailInnerContainer}>
+              <View style={style.restaurantNameRateContainer}>
+                <Text style={style.restaurantName}>{item.name}</Text>
+                <View style={style.restaurantRateContainer}>
+                  <Icon name="star" marginRight={3} size={20} />
+                  <View style={style.restaurantRateInnerContainer}>
+                    <Text style={style.restaurantActiveRate}>3.8</Text>
+                    <Text style={style.restaurantTotalRate}>/5</Text>
+                  </View>
+                </View>
+              </View>
+              <View style={style.restaurantAddressContainer}>
+                <Text style={style.restaurantAddress}>{item.address}</Text>
+                {/* <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                  <Icon name="star" marginRight={3} size={20} />
+                  <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <Text
+                      style={{
+                        color: '#2e2e2e',
+                        fontSize: 13,
+                        fontWeight: 'bold',
+                      }}>
+                      3.8
+                    </Text>
+                    <Text style={{color: '#d9d9d9'}}>/5</Text>
+                  </View>
+                </View> */}
+              </View>
+            </View>
+          </View>
+        </View>
+      </TouchableHighlight>
+    );
+  };
+
+  return (
+    <View style={style.container}>
+      {restaurants.length !== 0 ? (
+        <View style={style.flatlistContainer}>
+          <FlatList
+            contentContainerStyle={style.flatlistContentContainer}
+            data={restaurants}
+            renderItem={renderRestaurants}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        </View>
+      ) : (
+        <View />
+      )}
+    </View>
+  );
 }
 
 export default Home;
